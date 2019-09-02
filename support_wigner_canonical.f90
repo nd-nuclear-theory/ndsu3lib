8cdf1c38ba651b1a793aa9bc5f0bec310e08f73d
jherko <jherko@nd.edu>
2019-09-02 16:03:57 +0200

    wigner_canonical_extremal.f90 and support_wigner_canonical.f90 modified

--- support_wigner_canonical.f90
+++ support_wigner_canonical.f90
@@ -1,7 +1,7 @@
 SUBROUTINE support_wigner_canonical&
  (INC,su3irrep1,su3irrep2,NEC,NNC,KR0A,KR0B,DEWU3P,J1TA,IAB,ICD,INDMAX,DEWU3,KR0MAX)
 !---------------------------------------------------------------------------------
-! SUPPORT ROUTINE FOR SUBROUTINE calculate_extremal_SU3_SU2xU1_Wigner_coefficients
+! SUPPORT ROUTINE FOR SUBROUTINE wigner_canonical_extremal
 !---------------------------------------------------------------------------------
 !     UPDATE/MOD: (LSU,06-81)  J.P.DRAAYER        INDEXING DEWU3
 !                 (LSU,03-97)  J.P.DRAAYER        INTEGER OVERFLOW FIX
@@ -20,6 +20,10 @@ SUBROUTINE support_wigner_canonical&
 !
 ! INPUT ARGUMENTS: INC,su3irrep1,su3irrep2,NEC,NNC,KR0A,KR0B,DEWU3P,J1TA,IAB,ICD,INDMAX,KR0MAX
 ! OUTPUT ARGUMENT: DEWU3
+!
+! INC=0 corresponds to formula (18) and INC=1 corresponds to formula (21)
+! In the comments in expression like a(b) a corresponds to formula (18) and b corresponds to formula (21)
+! In formulae (18) and (21) "the first term" corresponds to Lambda'1(2)=Lambda1(2)+1/2 and "the second term" corresponds to Lambda'1(2)=Lambda1(2)-1/2
 !---------------------------------------------------------------------------------
 USE derived_types
 IMPLICIT NONE
@@ -39,7 +43,7 @@ INDPQ=-KR0MAX
 DO IIQ=1,NNC
  INDQ=INDQ+KR0MAX
  INDPQ=INDPQ+KR0MAX
- DO KR0=KR0A,KR0B
+ DO KR0=KR0A,KR0B    ! For INC=1: KR0A=KR0B=rho
   KI=KR0+INDQ
   KIP=KR0+INDPQ
   DEWU3(KI)=DEWU3P(KIP)
@@ -47,26 +51,26 @@ DO IIQ=1,NNC
 END DO
 IF(NEC==0)RETURN
 IF(INC==1)THEN
- L1=su3irrep2%lambda
- M1=su3irrep2%MU
- L2=su3irrep1%lambda
- M2=su3irrep1%mu
+ L1=su3irrep2%lambda ! L1=lam2
+ M1=su3irrep2%MU     ! M1=mu2
+ L2=su3irrep1%lambda ! L2=lam1
+ M2=su3irrep1%mu     ! M2=mu1
 ELSE
- L1=su3irrep1%lambda
- M1=su3irrep1%mu
- L2=su3irrep2%lambda
- M2=su3irrep2%mu
+ L1=su3irrep1%lambda ! L1=lam1
+ M1=su3irrep1%mu     ! M1=mu1
+ L2=su3irrep2%lambda ! L2=lam2
+ M2=su3irrep2%mu     ! M2=mu2
 END IF
-LL1=L1+1
-MM1=M1+1
-LL2=L2+1
-MM2=M2+1
-LM1=LL1+MM1
-LM2=LL2+MM2
-DO J2TD=1,NEC
- J1TD=NEC-J2TD
- J2D=J2TD-1
- J1D=J1TD+1
+LL1=L1+1             ! LL1=lam1(2)+1
+MM1=M1+1             ! MM1=mu1(2)+1
+LL2=L2+1             ! LL2=lam2(1)+1
+MM2=M2+1             ! MM2=mu2(1)+1
+LM1=LL1+MM1          ! LM1=lam1(2)+mu1(2)+2
+LM2=LL2+MM2          ! LM2=lam2(1)+mu2(1)+2
+DO J2TD=1,NEC        ! J2TD=(eps2-eps2_HW)/3+1 ?
+ J1TD=NEC-J2TD       ! J1TD=(eps1-eps1_HW)/3-1 ?
+ J2D=J2TD-1          ! J2D=(eps2-eps2_HW)/3 ?
+ J1D=J1TD+1          ! J1D=(eps1-eps1_HW)/3 ?
  IIQ2A=J2TD-M2
  IF(IIQ2A<0)IIQ2A=0
  IIQ2A=IIQ2A+1
@@ -82,8 +86,8 @@ DO J2TD=1,NEC
  DO IIQ2=IIQ2A,IIQ2B
   IQ2=IIQ2-1
   IP2=J2TD-IQ2
-  J2T=L2+IP2-IQ2
-  JJ2T=J2T+1
+  J2T=L2+IP2-IQ2                                  ! J2T=2*Lam'2(1) - see Eq.(1) for Lambda: 2*Lambda=lambda+\tilde{p}-\tilde{q}
+  JJ2T=J2T+1                                      ! JJ2T=2*Lam'2(1)+1
   IQ=-1
   IP=-1
   IF(IP2/=0)THEN
@@ -93,8 +97,8 @@ DO J2TD=1,NEC
    IP2D=1
    IF(INC==1)IP=1
 !B    NM=IP2*(M2-IP2P)*(LL2+IP2)                                        
-   DM=DFLOAT(IP2)*DFLOAT(M2-IP2P)*DFLOAT(LL2+IP2)
-   DN=DFLOAT(J2T)
+   DM=DFLOAT(IP2)*DFLOAT(M2-IP2P)*DFLOAT(LL2+IP2) ! Eq.(17): DM=S(q2(1)) where IP2P=\tilde{p2(1)}=mu2(1)-q2(1) and IP2=IP2P+1
+   DN=DFLOAT(J2T)                                 ! DN=2*Lam'2(1)=2*Lam2(1)+1
   ELSE
    IQ2P=IQ2-1
    IP2P=IP2
@@ -102,24 +106,24 @@ DO J2TD=1,NEC
    IP2D=0
    IF(INC==0)IQ=1
 !B    NM=IQ2*(L2-IQ2P)*(LM2-IQ2)                                        
-   DM=DFLOAT(IQ2)*DFLOAT(L2-IQ2P)*DFLOAT(LM2-IQ2)
-   DN=DFLOAT(J2T+2)
+   DM=DFLOAT(IQ2)*DFLOAT(L2-IQ2P)*DFLOAT(LM2-IQ2) ! Eq.(17): NM=R(p2(1)) where IQ2P=\tilde{q2(1)}=lam2(1)-p2(1) and IQ2=IQ2P+1
+   DN=DFLOAT(J2T+2)                               ! DN=2*Lam'2(1)+2=2*Lam2(1)+1
   END IF
   J2TP=L2+IP2P-IQ2P
   JTA=J2TD-IQ2P
   JTB=NNC-JTA
   NQD=NEC-IQ2P
   DO IIQ1=IIQ1A,IIQ1B
-   IQ1=IIQ1-1
-   IP1=J1TD-IQ1
-   J1T=L1+IP1-IQ1
+   IQ1=IIQ1-1                                     ! Eq.(18)((21)): IQ1=\tilde{q1(2)}=lam1(2)-p1(2)
+   IP1=J1TD-IQ1                                   ! Eq.(18)((21)): IP1=\tilde{p1(2)}=mu1(2)-q1(2)
+   J1T=L1+IP1-IQ1                                 ! J1T=2*Lam1(2) - see formula (1) for Lambda
    IF(INC==0)IND=INDEX(J1TD,L1,J1T,J2TD,L2,J2T)
    IF(INC==1)IND=INDEX(J2TD,L2,J2T,J1TD,L1,J1T)
    IF(J1TA(IND)<0)CYCLE
    IF(IP1/=M1)THEN
     IQ1P=IQ1
-    IP1P=IP1+1
-    J1TP=J1T+1
+    IP1P=IP1+1                                    ! Eq.(18)((21)): IP1P=\tilde{p1(2)}+1=mu1(2)-q1(2)+1
+    J1TP=J1T+1                                    ! J1TP=2*Lam1(2)+1
     IF(INC/=0)THEN
      INDP=INDEX(J2D,L2,J2TP,J1D,L1,J1TP)
      IF(J1TA(INDP)>=0)THEN
@@ -138,14 +142,14 @@ DO J2TD=1,NEC
 	 END IF
 	END IF
 	IF(IHELP==1)THEN
-     IF(IP2D==1)I=IAB(J123)
-     IF(IQ2D==1)I=ICD(NQD-IQ1)
+     IF(IP2D==1)I=IAB(J123)                       ! Eq.(18)/(21): I=[A(...)+1/2][B(...)+1/2]
+     IF(IQ2D==1)I=ICD(NQD-IQ1)                    ! Eq.(18)/(21): I=[C(...)+1/2][D(...)+1/2]
 !     I=JJ2T*IP1P*(MM1-IP1P)*(LL1+IP1P)*I
 !     DC=DSQRT(DFLOAT(I)/(DFLOAT((J1T+2)*J1TP*NM)*DN))
 !B    DX=DFLOAT(JJ2T*IP1P*(MM1-IP1P))*DFLOAT((LL1+IP1P)*I)
 !B    DC=DSQRT(DX/(DFLOAT((J1T+2)*J1TP*NM)*DN))                  
-     DX=DFLOAT(JJ2T)*DFLOAT(IP1P)*DFLOAT(MM1-IP1P)*DFLOAT((LL1+IP1P)*I)
-     DC=DSQRT(DX/(DFLOAT((J1T+2)*J1TP)*DM*DN))                  
+     DX=DFLOAT(JJ2T)*DFLOAT(IP1P)*DFLOAT(MM1-IP1P)*DFLOAT((LL1+IP1P)*I) ! Eq.(18)((21)): DX=(2*Lam'2(1)+1)*(X(Y)(Lam'1,Lam'2))^2 in the first term on the right-hand side; where JJ2T=2*Lam'2(1)+1 and the rest is X(Y)^2, where IP1P*(MM1-IP1P)*(LL1+IP1P)=S(q1(2)) - in the first term there is always S(q1(2))
+     DC=DSQRT(DX/(DFLOAT((J1T+2)*J1TP)*DM*DN))                          ! Eq.(18)((21)): DC=factor of the Wigner coefficient in the first term on the right-hand side; where DM=N(Lam'1(2)) and DN=2*Lam2(1)+1
 !W    write(6,*) ' line', 40, dx, dc
      IF(IQ<0)DC=-DC
      INDQ=(IND-1)*KR0MAX
@@ -153,14 +157,14 @@ DO J2TD=1,NEC
      DO KR0=KR0A,KR0B
       KI=KR0+INDQ
       KIP=KR0+INDPQ
-      DEWU3(KI)=DC*DEWU3(KIP)
+      DEWU3(KI)=DC*DEWU3(KIP)                     ! Eq.(18)/(21): DEWU3(KI)=the first term on the right-hand side
      END DO
     END IF
    END IF
    IF(IQ1==L1)CYCLE
-   IQ1P=IQ1+1
+   IQ1P=IQ1+1                                     ! Eq.(18)((21)): IQ1P=\tilde{q1(2)}+1=lam1(2)-p1(2)+1
    IP1P=IP1
-   J1TP=J1T-1
+   J1TP=J1T-1                                     ! J1TP=2*Lam1(2)-1
    IF(INC/=0)THEN
     INDP=INDEX(J2D,L2,J2TP,J1D,L1,J1TP)
     IF(J1TA(INDP)<0)CYCLE
@@ -170,14 +174,14 @@ DO J2TD=1,NEC
     IF(J1TA(INDP)<0)CYCLE
     J123=JTA+IQ1
    END IF
-   IF(IP2D==1)I=ICD(NQD-IQ1)
-   IF(IQ2D==1)I=IAB(J123)
+   IF(IP2D==1)I=ICD(NQD-IQ1)                      ! Eq.(18)/(21): I=[C(...)+1/2][D(...)+1/2]
+   IF(IQ2D==1)I=IAB(J123)                         ! Eq.(18)/(21): I=[A(...)+1/2][B(...)+1/2]
 !     I=JJ2T*IQ1P*(LL1-IQ1P)*(LM1-IQ1P)*I
 !     DC=DSQRT(DFLOAT(I)/(DFLOAT((J1TP+2)*J1T*NM)*DN))
 !B    DX=DFLOAT(JJ2T*IQ1P*(LL1-IQ1P))*DFLOAT((LM1-IQ1P)*I)
 !B    DC=DSQRT(DX/(DFLOAT((J1TP+2)*J1T*NM)*DN))                  
-   DX=DFLOAT(JJ2T)*DFLOAT(IQ1P)*DFLOAT(LL1-IQ1P)*DFLOAT((LM1-IQ1P)*I)
-   DC=DSQRT(DX/(DFLOAT((J1TP+2)*J1T)*DM*DN))                  
+   DX=DFLOAT(JJ2T)*DFLOAT(IQ1P)*DFLOAT(LL1-IQ1P)*DFLOAT((LM1-IQ1P)*I) ! Eq.(18)((21)): DX=(2*Lam'2(1)+1)*(X(Y)(Lam'1,Lam'2))^2 in the second term on the right-hand side; where JJ2T=2*Lam'2(1)+1 and the rest is X(Y)^2, where IQ1P*(LL1-IQ1P)*(LM1-IQ1P)=R(p1(2)) - in the second term there is always R(p1(2))
+   DC=DSQRT(DX/(DFLOAT((J1TP+2)*J1T)*DM*DN))                          ! Eq.(18)((21)): DC=factor of the Wigner coefficient in the second term on the right-hand side; where DM=N(Lam'1(2)) and DN=2*Lam2(1)+1
 !W    write(6,*) ' line', 60, dx, dc
    IF(IP<0)DC=-DC
    INDQ=(IND-1)*KR0MAX
@@ -185,7 +189,7 @@ DO J2TD=1,NEC
    DO KR0=KR0A,KR0B
     KI=KR0+INDQ
     KIP=KR0+INDPQ
-    DEWU3(KI)=DEWU3(KI)+DC*DEWU3(KIP)
+    DEWU3(KI)=DEWU3(KI)+DC*DEWU3(KIP)             ! Eq.(18)/(21): DEWU3(KI)=Wigner coefficient on the left-hand side
    END DO
   END DO
  END DO
--- wigner_canonical_extremal.f90
+++ wigner_canonical_extremal.f90
@@ -13,7 +13,7 @@ SUBROUTINE wigner_canonical_extremal&
 !                 (UoT,04-97)  C.BAHRI            NX=82
 !                 (ND,2019)    J.HERKO            REWRITTEN IN MODERN FORTRAN, MODULE INSTEAD OF COMMON BLOCKS, EXPLICIT DECLARATIONS,
 !                                                 STATEMENT FUNCTION "INDEX" REPLACED BY INTERNAL SUBPROGRAM, DERIVED DATA TYPE su3irrep
-!                                                 INSTEAD OF LAMBDA AND MU, AUTOMATIC ARRAYS
+!                                                 INSTEAD OF LAMBDA AND MU, AUTOMATIC ARRAYS, MINOR CHANGES
 !                                                                       
 !     REFERENCES--J.P.DRAAYER AND Y.AKIYAMA, J.MATH.PHYS.14(1973)1904   
 !                 K.T.HECHT, NUCL.PHYS.62(1965)1                        
@@ -48,12 +48,14 @@ SUBROUTINE wigner_canonical_extremal&
 ! NX=lam2+mu2+1
 ! NEC=n=(lam1+lam2-lam3+2*(mu1+mu2-mu3))/3
 ! KR0MAX=MULTIPLICITY OF COUPLING (lam1,mu1)x(lam2,mu2)->(lam3,mu3)
-! J1TA(IND)=2*Lam1 WHERE 1<=IND<=INDMAX
+! J1TA(IND)=2*Lam1 WHERE 1<=IND<=INDMAX=(n+1)(n+2)(n+3)/6
 ! J2TA(IND)=2*Lam2 WHERE 1<=IND<=INDMAX
 ! IEA(IND)=eps2 WHERE 1<=IND<=INDMAX
 ! eps1=eps3E-eps2
 ! FOR GIVEN eps1,Lam1,eps2,Lam2 THE EXTREMAL REDUCED WIGNER COEFFICIENTS ARE THE ELEMENTS OF ARRAY
 !  DEWU3 WITH INDECES rho+KR0MAX*(IND-1)
+!
+! The comments perain to the I3=1 case only!
 !-----------------------------------------------------------------------
 USE derived_types
 USE binomial_coeff_factorials
@@ -112,27 +114,33 @@ ELSE
  su3irrep2%mu=su3irrep2x%lambda
  su3irrep3%mu=su3irrep3x%lambda
 END IF
-NEC=(su3irrep1%lambda+su3irrep2%lambda-su3irrep3%lambda+2*(su3irrep1%mu+su3irrep2%mu-su3irrep3%mu))/3
-IAH=(su3irrep2%lambda+su3irrep3%lambda-su3irrep1%lambda-NEC)/2
-IBH=(su3irrep3%lambda+su3irrep1%lambda-su3irrep2%lambda+NEC+2)/2
-ICH=(su3irrep1%lambda+su3irrep2%lambda-su3irrep3%lambda-NEC)/2
-IDH=(su3irrep1%lambda+su3irrep2%lambda+su3irrep3%lambda-NEC+2)/2
+NEC=(su3irrep1%lambda+su3irrep2%lambda-su3irrep3%lambda+2*(su3irrep1%mu+su3irrep2%mu-su3irrep3%mu))/3 ! NEC=n=(lam1+lam2-lam3+2*(mu1+mu2-mu3))/3
+IAH=(su3irrep2%lambda+su3irrep3%lambda-su3irrep1%lambda-NEC)/2   ! IAH=a in Eq.(20) ?
+IBH=(su3irrep3%lambda+su3irrep1%lambda-su3irrep2%lambda+NEC+2)/2 ! IBH=b in Eq.(20) ?
+ICH=(su3irrep1%lambda+su3irrep2%lambda-su3irrep3%lambda-NEC)/2   ! ICH=c in Eq.(20) ?
+IDH=(su3irrep1%lambda+su3irrep2%lambda+su3irrep3%lambda-NEC+2)/2 ! IDH=d in Eq.(20) ?
 DO I=1,NEC
- IAB(I)=(IAH+I)*(IBH-I)
- ICD(I)=(ICH+I)*(IDH+I)
+ IAB(I)=(IAH+I)*(IBH-I) ! IAB(I)=(a+I)*(b-I)
+ ICD(I)=(ICH+I)*(IDH+I) ! ICD(I)=(c+I)*(d+I)
 END DO
-NCDMAX=outer_multiplicity_theory(su3irrep1,su3irrep2,su3irrep3) ! outer_multiplicity_theory >= outer_multiplicity
-NEC=NEC-NCDMAX
-su3irrep2=su3irrep2+(-NCDMAX) ! Operator + (defined in module derived_types) adds an integer to lambda and mu
+NCDMAX=outer_multiplicity_theory(su3irrep1,su3irrep2,su3irrep3) ! NCDMAX>=eta_max
+! outer_multiplicity_theory>=outer multiplicity
+NEC=NEC-NCDMAX ! NEC<=n-eta_max
+su3irrep2=su3irrep2+(-NCDMAX) ! su3irrep2%lambda<=lam2-eta_max, su3irrep2%mu<=mu2-eta_max
+! Operator + (defined in module derived_types) adds an integer to lambda and mu
 NCDMIN=1
 DO WHILE ((NCDMIN/=NCDMAX).AND.(outer_multiplicity(su3irrep1,su3irrep2+1,su3irrep3)<=0))
  NEC=NEC+1
  su3irrep2=su3irrep2+1
  NCDMIN=NCDMIN+1
 END DO
+! NEC=n-eta_max
+! su3irrep2%lambda=lam2-eta_max, su3irrep2%mu=mu2-eta_max
+! NCDMAX-NCDMIN+1=eta_max
+
 ! DIMENSION MODIFICATION (LSU,6-81)-START
-NNCMAX=NEC+NCDMAX-NCDMIN+2
-KITEST=KR0MAX*(NNCMAX)*(NNCMAX+1)*(NNCMAX+2)/6
+NNCMAX=NEC+NCDMAX-NCDMIN+2 ! NNCMAX=n+1
+KITEST=KR0MAX*(NNCMAX)*(NNCMAX+1)*(NNCMAX+2)/6 ! KITEST=rho_max*(n+1)*(n+2)*(n+3)/6=rho_max*INDMAX=maximal needed index of DEWU3=minimal needed size of DEWU3
 !*************************************************************************** BEGINNING OF A BLOCK IRRELEVANT FOR AUTOMATIC DEWU3
 !IF(KITEST>KIMAX1)THEN !DIMENSION CHECKS (LSU,6-81)
 ! WRITE(*,FMT='(40H ***** XEWU3 DIMENSION OVERFLOW: KITEST=,I10,5X,7HKIMAX1=,I10)')KITEST,KIMAX1
@@ -140,42 +148,43 @@ KITEST=KR0MAX*(NNCMAX)*(NNCMAX+1)*(NNCMAX+2)/6
 !END IF
 ! DIMENSION MODIFICATION (LSU,6-81)--STOP
 !*************************************************************************** END OF THE BLOCK IRRELEVANT FOR AUTOMATIC DEWU3
+
 DO I=1,KITEST
  DEWU3(I)=0.D0
 ENDDO
-LL1=su3irrep1%lambda+1
-MM1=su3irrep1%mu+1
-LL2=su3irrep2%lambda+1
-MM2=su3irrep2%mu+1
-IA1=2*su3irrep1%lambda+4*su3irrep1%mu
-IB1=4*su3irrep1%lambda+2*su3irrep1%mu
-IC1=IB1-IA1
-IA2=2*su3irrep2%lambda+4*su3irrep2%mu
-IB2=4*su3irrep2%lambda+2*su3irrep2%mu
-IC2=IB2-IA2
-IS1=LL1+MM1
-IS2=LL2+MM2
-ISS=MM1+su3irrep2%lambda+su3irrep2%mu-NEC
-IE3=-(su3irrep3%lambda+2*su3irrep3%mu)
-IEH=-(su3irrep2%lambda+2*su3irrep2%mu+3)
+LL1=su3irrep1%lambda+1                    ! LL1=lam1+1
+MM1=su3irrep1%mu+1                        ! MM1=mu1+1
+LL2=su3irrep2%lambda+1                    ! LL2=lam2-eta_max+1
+MM2=su3irrep2%mu+1                        ! MM2=mu2-eta_max+1
+IA1=2*su3irrep1%lambda+4*su3irrep1%mu     ! IA1=2*lam1+4*mu1
+IB1=4*su3irrep1%lambda+2*su3irrep1%mu     ! IB1=4*lam1+2*mu1
+IC1=IB1-IA1                               ! IC1=2*lam1-2*mu1
+IA2=2*su3irrep2%lambda+4*su3irrep2%mu     ! IA2=2*(lam2-eta_max)+4*(mu2-eta_max)=2*lam2+4*mu2-6*eta_max
+IB2=4*su3irrep2%lambda+2*su3irrep2%mu     ! IB2=4*(lam2-eta_max)+2*(mu2-eta_max)=4*lam2+2*mu2-6*eta_max
+IC2=IB2-IA2                               ! IC2=2*(lam2-eta_max)-2*(mu2-eta_max)=2*lam2-2*mu2
+IS1=LL1+MM1                               ! IS1=lam1+mu1+2
+IS2=LL2+MM2                               ! IS2=lam2-eta_max+mu2-eta_max+2=lam2+mu2-2*eta_max
+ISS=MM1+su3irrep2%lambda+su3irrep2%mu-NEC ! ISS=mu1+1+lam2-eta_max+mu2-eta_max-n+eta_max=mu1+1+lam2+mu2-n-eta_max
+IE3=-(su3irrep3%lambda+2*su3irrep3%mu)    ! IE3=-lam3-2*mu3=eps3_min=eps3_HW
+IEH=-(su3irrep2%lambda+2*su3irrep2%mu+3)  ! IEH=-lam2-eta_max-2*(mu2-eta_max)-3=-lam2-2*mu2+eta_max-3=eps2_HW+eta_max-3
 KR0CNT=0
-DO NCD=NCDMIN,NCDMAX
- NEC=NEC+1
- su3irrep2=su3irrep2+1
- NNC=NEC+1
- INDMAX=NNC*(NNC+1)*(NNC+2)/6
- IA2=IA2+6
- IB2=IB2+6
- IS2=IS2+2
- ISS=ISS+1
- IEH=IEH-3
- LL2=su3irrep2%lambda+1
- MM2=su3irrep2%mu+1
- LN1=su3irrep1%lambda+NEC
- LN2=su3irrep2%lambda+NEC
- INN=NEC*NNC/2
- IF(NCD/=NCDMIN)THEN
-  DO I=1,KITEST
+DO NCD=NCDMIN,NCDMAX ! This loop is a loop over eta from eta_max-1 to 0 (which is related to rho=eta_max-eta).
+ NEC=NEC+1                    ! NEC=n-eta=\bar{n}=(lam1+\bar{lam2}-lam3+2*(mu1+\bar{mu2}-mu3))/3 (see the bottom of Eq.(20))
+ su3irrep2=su3irrep2+1        ! su3irrep2%lambda=lam2-eta=\bar{lam2}, su3irrep2%mu=mu2-eta=\bar{mu2}
+ NNC=NEC+1                    ! NNC=n-eta+1=\bar{n}+1
+ INDMAX=NNC*(NNC+1)*(NNC+2)/6 ! INDMAX=INDMAX for \bar{lam2} and \bar{mu2}
+ IA2=IA2+6                    ! IA2=2*lam2+4*mu2-6*eta=2*\bar{lam2}+4*\bar{mu2}
+ IB2=IB2+6                    ! IB2=4*lam2+2*mu2-6*eta=4*\bar{lam2}+2*\bar{mu2}
+ IS2=IS2+2                    ! IS2=lam2+mu2-2*eta=\bar{lam2}+\bar{mu2}
+ ISS=ISS+1                    ! ISS=mu1+1+lam2+mu2-n-eta=mu1+1+\bar{lam2}+\bar{mu2}-\bar{n}
+ IEH=IEH-3                    ! IEH=\bar{eps2}_min-3=\bar{eps2}_HW-3=eps2_HW+eta-3
+ LL2=su3irrep2%lambda+1       ! LL2=\bar{lam2}+1
+ MM2=su3irrep2%mu+1           ! \bar{mu2}+1
+ LN1=su3irrep1%lambda+NEC     ! LN1=lam1+\bar{n}
+ LN2=su3irrep2%lambda+NEC     ! LN2=\bar{lam2}+\bar{n}
+ INN=NEC*NNC/2                ! INN=\bar{n}*(\bar{n}+1)/2
+ IF(NCD/=NCDMIN)THEN ! If NCD=NCDMIN, DEWU3 does not need to be zeroized because it already is zeroized - see above
+  DO I=1,KR0MAX*INDMAX ! KITEST used to be here instead of KR0MAX*INDMAX (<=KITEST)
    DEWU3(I)=0.D0
   END DO
  END IF
@@ -184,15 +193,15 @@ DO NCD=NCDMIN,NCDMAX
   J2TA(IND)=-1000
   J1TA(IND)=-1000
  END DO
- IE2=IEH
+ IE2=IEH ! IE2=\bar{eps2}_HW-3
  I=1000
  DO IIE=1,NNC
-  IE2=IE2+3
-  IE1=IE3-IE2
-  J2TD=IIE-1
-  J1TD=NNC-IIE
-  JJ2TA=IA2-IE2
-  JJ2TB=IB2+IE2
+  IE2=IE2+3 ! IE2=\bar{eps2}
+  IE1=IE3-IE2 ! IE1=eps3_HW-\bar{eps2}=eps1
+  J2TD=IIE-1 ! J2TD=(\bar{eps2}-\bar{eps2}_HW)/3=0,...,\bar{n}
+  J1TD=NNC-IIE ! J1TD=(eps1-eps1_HW)/3=\bar{n},...,0 , It holds: J1TD+J2TD=\bar{n}
+  JJ2TA=IA2-IE2 ! JJ2TA=2*\bar{lam2}+4*\bar{mu2}-\bar{eps2}
+  JJ2TB=IB2+IE2 ! JJ2TB=4*\bar{lam2}+2*\bar{mu2}+\bar{eps2}
   IF(JJ2TB<JJ2TA)JJ2TA=JJ2TB
   JJ2TA=JJ2TA/3+1
   JJ2TB=JJ2TA-ABS(IC2-IE2)/3
@@ -203,17 +212,17 @@ DO NCD=NCDMIN,NCDMAX
   JJ1TB=JJ1TA-ABS(IC1-IE1)/3
   J=0
   DO JJ2T=1,JJ2TB,2
-   J2T=JJ2TA-JJ2T
-   L=ABS(J2T-su3irrep3%lambda)
-   M=J2T+su3irrep3%lambda
+   J2T=JJ2TA-JJ2T ! J2T=2*\bar{Lam2}
+   L=ABS(J2T-su3irrep3%lambda) ! L=the lowest possible 2*Lam1 according to the triangular rule: 2*Lam3_HW=lam3
+   M=J2T+su3irrep3%lambda ! M=the greatest possible 2*Lam1 according to the triangular rule
    DO JJ1T=1,JJ1TB,2                                              
-    J1T=JJ1TA-JJ1T
+    J1T=JJ1TA-JJ1T ! J1T=2*Lam1
     IF(J1T<L)CYCLE
     IF(J1T>M)CYCLE
-    IND=INDEX(J1TD,su3irrep1%lambda,J1T,J2TD,su3irrep2%lambda,J2T)
-    IEA(IND)=IE2
-    J2TA(IND)=J2T
-    J1TA(IND)=J1T
+    IND=INDEX(J1TD,su3irrep1%lambda,J1T,J2TD,su3irrep2%lambda,J2T) ! IND is a reference to <(lam1,mu1)eps1,Lam1;(bar{lam2},bar{mu2})\bar{eps2},\bar{Lam2}||(lam1,mu1)HW>_rho
+    IEA(IND)=IE2 ! IEA(IND)=\bar{eps2}
+    J2TA(IND)=J2T ! J2TA(IND)=2*\bar{Lam2}
+    J1TA(IND)=J1T ! J1TA(IND)=2*Lam1
     J=J+1
    END DO
   END DO
@@ -228,35 +237,35 @@ DO NCD=NCDMIN,NCDMAX
   INDQ=-KR0MAX
   DO IND=1,NNC
    INDQ=INDQ+KR0MAX
-   J1T=J1TA(IND)                                                     
+   J1T=J1TA(IND)                               ! Eq.(17): J1T=2*Lam1
    IF(J1T<0)CYCLE
-   IQ1=(LN1-J1T)/2
+   IQ1=(LN1-J1T)/2                             ! Eq.(17): IQ1=\tilde{q_1}=lam1-p_1
    IF(IQ1>0)THEN
-    J1TP=J1T+1
+    J1TP=J1T+1                                 ! Eq.(17): J1TP=2*Lam1+1
     INDP=(LN1-J1TP-1)/2+1
     IF(J1TAP(INDP)>=0)THEN
-     I=IAB(IQ1)*IQ1*(LL1-IQ1)*(IS1-IQ1)
-     DC=-DSQRT(DFLOAT(I)/DFLOAT((J1T+2)*J1TP))
+     I=IAB(IQ1)*IQ1*(LL1-IQ1)*(IS1-IQ1)        ! Eq.(17): I=numerator in the first square root: IAB(IQ1)=A(...)[B(...)+1] (because Lam1=-lam1/2+n/2+p_1), (LL1-IQ1)*IQ1*(IS1-IQ1)=R(p_1+1)
+     DC=-DSQRT(DFLOAT(I)/DFLOAT((J1T+2)*J1TP)) ! Eq.(17): DC=-the first square root
      INDPQ=(INDP-1)*KR0MAX
      DO KR0=1,KR0CNT
       KI=KR0+INDQ
       KIP=KR0+INDPQ
-      DEWU3(KI)=DC*DEWU3P(KIP)
+      DEWU3(KI)=DC*DEWU3P(KIP)                 ! Eq.(17): DEWU3(KI)=the first term on the right-hand side
      END DO
     END IF
    END IF
-   IP1=NEC-IQ1
+   IP1=NEC-IQ1                                 ! Eq.(17): IP1=\tilde{p_1}=mu1-q_1
    IF(IP1<=0)CYCLE
-   J1TP=J1T-1
+   J1TP=J1T-1                                  ! Eq.(17): J1TP=2*Lam1-1
    INDP=(LN1-J1TP-1)/2+1
    IF(J1TAP(INDP)<0)CYCLE
-   I=ICD(NNC-IND)*IP1*(MM1-IP1)*(LL1+IP1)
-   DC=DSQRT(DFLOAT(I)/DFLOAT((J1TP+2)*J1T))
+   I=ICD(NNC-IND)*IP1*(MM1-IP1)*(LL1+IP1)      ! Eq.(17): I=numerator in the second square root: ICD(NNC-IND)=C(...)D(...), (MM1-IP1)*IP1*(LL1+IP1)=S(q_1+1)
+   DC=DSQRT(DFLOAT(I)/DFLOAT((J1TP+2)*J1T))    ! Eq.(17): DC=the second square root
    INDPQ=(INDP-1)*KR0MAX
    DO KR0=1,KR0CNT
     KI=KR0+INDQ
     KIP=KR0+INDPQ
-    DEWU3(KI)=DEWU3(KI)+DC*DEWU3P(KIP)
+    DEWU3(KI)=DEWU3(KI)+DC*DEWU3P(KIP)         ! Eq.(17): DEWU3(KI)=Wigner coefficient on the left-hand side
    END DO
   END DO
   INC=0
@@ -266,9 +275,9 @@ DO NCD=NCDMIN,NCDMAX
 !     EVALUATE <(LAM1,MU1)HIGH;(LAM2,MU2)????::KR0(LAM3,MU3)HIGH>
 !     WITH (LAM2,MU2) A MINIMUM FOR KR0=KR0CNT
 
-  KR0CNT=KR0CNT+1
+  KR0CNT=KR0CNT+1              ! KR0CNT=rho=1,...,rho_max
   I=0
-  IND=INDEX(0,su3irrep1%lambda,su3irrep1%lambda,NEC,su3irrep2%lambda,LN2)-1
+  IND=INDEX(0,su3irrep1%lambda,su3irrep1%lambda,NEC,su3irrep2%lambda,LN2)-1 ! IND is a reference to <(lam1,mu)HW;(\bar{lam2},\bar{mu2})3*\bar{n}+\bar{eps2}_HW,LN2/2||(lam3,mu3)HW>_rho
   INDQ=-KR0MAX
   DO IIQ2=1,NNC                                                  
    INDQ=INDQ+KR0MAX
@@ -277,26 +286,26 @@ DO NCD=NCDMIN,NCDMAX
    DEWU3P(KI)=0.D0
    IF(J1TA(IND)<0)CYCLE
    I=I+1
-   IIQ2B=IIQ2
+   IIQ2B=IIQ2                  ! Eq.(20): IIQ2B=j_2+1 ?
   END DO
 !                                                                       
 !     *****MODIFIED TO AVOID OVERFLOW (LSU,5-80)-START                  
 !      ****DLOGB CHANGED TO SAVE SPACE (LSU,3-83)****                   
 !      ****FURTHER OVERFLOW CORRECTION (LSU,2-87)****                   
 !                                                                       
-  IIQ2A=IIQ2B-I+1
-  IQ2B=IIQ2B-1
+  IIQ2A=IIQ2B-I+1              ! Eq.(20): IIQ2A=j_1+1 ?
+  IQ2B=IIQ2B-1                 ! Eq.(20): IQ2B=j_2 ?
   INDQ=(IIQ2A-2)*KR0MAX
   IZ=0
-  DO IIQ2=IIQ2A,IIQ2B
+  DO IIQ2=IIQ2A,IIQ2B          ! Eq.(20): IIQ2=\tilde{\bar{q_2}}+1
    IZ=IZ+1
    DZ(IZ)=1.D0
    INDQ=INDQ+KR0MAX
-   L=LL2-IIQ2
+   L=LL2-IIQ2                  ! Eq.(20): L=(\bar{lam2}+1)-(\tilde{\bar{q_2}}+1)=\bar{lam2}-\tilde{\bar{q_2}} ?
 !                                                                       
 ! --> START NUMERATOR PRODUCT LOOP                                      
 !                                                                       
-   IX=L-IIQ2+NNC+1
+   IX=L-IIQ2+NNC+1             ! Eq.(20): IX=the expression in front of the binomial coefficient in G
    IF(IX==0)THEN !"GO TO 120" was originally here
 	IHELP=0 !IHELP is an indicator of how the end of the loop was reahed (if via EXIT then IHELP=0 else IHELP=1)
 	EXIT
@@ -304,20 +313,20 @@ DO NCD=NCDMIN,NCDMAX
 	IHELP=1
    END IF
    IY=ABS(IX)
-   IN=IX/IY
-   DN=DLOG(DFLOAT(IY))
-   IF(IIQ2A/=IIQ2B)THEN
-    DO I=IIQ2A,IQ2B
-     J=NNC-I
-     IF(I<IIQ2)THEN
-      K=IAB(J)*ICD(J)*(IS2-I)
-     ELSE
-      K=MM2-J
+   IN=IX/IY                    ! IN=the sign of IX
+   DN=DLOG(DFLOAT(IY))         ! DN=log(|IX|)
+   IF(IIQ2A/=IIQ2B)THEN        ! If j_2/=j_1, G is calculated. If j_2=j_1, G=1 i.e. there is no G.
+    DO I=IIQ2A,IQ2B            ! Eq.(20): I=j+1 in g(j) ; This loop calculates the logarithm of G in Eq.(20) without the binomial coefficient.
+     J=NNC-I                   ! Eq.(20): J=(\bar{n}+1)-(j+1)=\bar{n}-j
+     IF(I<IIQ2)THEN            ! if j<\tilde{\bar{q_2}}
+      K=IAB(J)*ICD(J)*(IS2-I)  ! Eq.(20): K=g(j)=... for j<\tilde{\bar{q_2}}
+     ELSE                      ! if j>=\tilde{\bar{q_2}}
+      K=MM2-J                  ! Eq.(20): K=g(j)=... for j>=\tilde{\bar{q_2}}
      ENDIF
      IF(K==0)EXIT !"GO TO 120" was originally here
      IF(K<0)IN=-IN
      DN=DLOG(DFLOAT(ABS(K)))+DN
-    END DO
+    END DO                     ! DN=the logarithm of G in Eq.(20) without the binomial coefficient
     IF(K==0)THEN
 	 IHELP=0
 	 EXIT
@@ -325,48 +334,48 @@ DO NCD=NCDMIN,NCDMAX
 !	 IHELP=1 !This is probably redundant
 	END IF
    END IF
-   DN=DN+DLOG(DBINO(INN+IIQ2))
+   DN=DN+DLOG(DBINO(INN+IIQ2)) ! DN=the logarithm of G in Eq.(20)
 !                                                                       
 ! --> END NUMERATOR PRODUCT LOOP & START DENOMINATOR PRODUCT LOOP       
 !                                                                       
    ID=1
    DD=0.D0
-   DO I=1,NNC
+   DO I=1,NNC                  ! I=1,...,\bar{n}+1
     IX=I+L
     IF(IX<0)ID=-ID
-    DD=DLOG(DFLOAT(I+L))+DD
-   END DO
+    DD=DLOG(DFLOAT(I+L))+DD    ! DD=log(L+1)+log(L+2)+...+log(L+\bar{n}+1)=log((L+\bar{n}+1)*(L+\bar{n})*...*(L+1))=log((L+\bar{n}+1)!/L!)
+   END DO                      ! Eq.(20): DD=log(H*(\bar{n}+1)!) ?????
 !                                                                       
 ! --> END DENOMINATOR PRODUCT LOOP & START INNER PRODUCT/SUM LOOP       
 !                                                                       
-   IP2=NNC-IIQ2
+   IP2=NNC-IIQ2                ! Eq.(20): IP2=\tilde{\bar{p_2}} (=\bar{n}-\tilde{\bar{q_2}})
 !                                                                       
 !     MULTIPLY BY SMALL NUMBER --> DEXP(-172) LIMIT FOR IBM SYSTEMS     
 !                                                                       
    DZ(IZ)=DEXP(-DMIN1(DLOGF(2*IP2),172.D0))
-   IIP2=IP2+1
-   M=IP2*IIP2/2
+   IIP2=IP2+1                  ! Eq.(20): IIP2=\tilde{\bar{p_2}}+1
+   M=IP2*IIP2/2                ! Eq.(20): M=\tilde{\bar{p_2}}*(\tilde{\bar{p_2}}+1)/2
    DS=0.D0
-   DO I=1,IIP2
-    DC=DZ(IZ)*DBINO(I+M)
+   DO I=1,IIP2                 ! Eq.(20): I=i+1 ; This loop calculates the sum in F in Eq.(20)
+    DC=DZ(IZ)*DBINO(I+M)       ! Eq.(20): DC=binomial coefficient in F multiplied by DZ(IZ) whose effect is renormalized below
     IF(IIP2/=1)THEN
-     DO J=1,IP2
-      IF(J<I)THEN
-       K=(J+L)*(ISS+J)
-      ELSE
-       K=IAB(J)
+     DO J=1,IP2                ! Eq.(20): J=j+1 (j is the index in the product in F) ; This loop calculates the binomial coefficient times the product in F in Eq.(20)
+      IF(J<I)THEN              ! if j<i
+       K=(J+L)*(ISS+J)         ! Eq.(20): K=f(j)=... for j<i (L=\bar{lam2}-\tilde{\bar{q_2}}=\bar{p_2})
+      ELSE                     ! if j>=i
+       K=IAB(J)                ! Eq.(20): K=f(j)=... for j>=i
       ENDIF
       DC=DFLOAT(K)*DC
-     END DO
+     END DO                    ! Eq.(20): DC=the binomial coefficient times the product in F
     END IF
     DS=DS+DC
-   END DO
+   END DO                      ! Eq.(20): DS=the sum in F
 !                                                                       
 ! --> END INNER PRODUCT/SUM LOOP & ASSIGN unnormalized DEWU3P VALUE     
 !                                                                       
-   IF(2*(IP2/2)/=IP2)DS=-DS
+   IF(2*(IP2/2)/=IP2)DS=-DS    ! Eq.(20): DS=F
    KI=KR0CNT+INDQ
-   DEWU3P(KI)=DFLOAT(IN*ID)*DS*DEXP((DN-DD)/2.D0)
+   DEWU3P(KI)=DFLOAT(IN*ID)*DS*DEXP((DN-DD)/2.D0) ! Eq.(20): DEWU3P(KI)=the unnormalized Wigner coefficient (IN and ID take care of the sign)
   END DO
   
   IF(IHELP/=0)THEN ! IHELP is an indicator of how the end of the loop was reahed (if via EXIT then IHELP=0)
@@ -386,13 +395,13 @@ DO NCD=NCDMIN,NCDMAX
     IZ=IZ+1
     INDQ=INDQ+KR0MAX
     KI=KR0CNT+INDQ
-    DEWU3P(KI)=(DMIN/DZ(IZ))*DEWU3P(KI)
+    DEWU3P(KI)=(DMIN/DZ(IZ))*DEWU3P(KI) ! Eq.(20): DEWU3P(KI)=the Wigner coefficient
    END DO
 !                                                                       
 !     *****MODIFIED TO AVOID OVERFLOW (LSU,5-80)--STOP                  
 !                                                                       
-   KR0A=KR0CNT
-   KR0B=KR0CNT
+   KR0A=KR0CNT ! KR0A=rho
+   KR0B=KR0CNT ! KR0B-rho
    
 !     GENERATE <(LAM1,MU1)????;(LAM2,MU2)????::KR0(LAM3,MU3)HIGH>
 !     FROM <(LAM1,MU1)HIGH;(LAM2,MU2)????::KR0(LAM3,MU3)HIGH>
