cc86a5396708310c26d46d1d63bcd933f7a7f19c
Anna <amccoy@nd.edu>
2022-03-30 00:01:21 -0700

    cmake: Now builds with wigxjpf and mpfun20.

---
+++ retrieve.cmake
@@ -0,0 +1,49 @@
+###############################################################################
+# Defintions of FetchContent and function to fetch if package not found.
+#
+# Anna E. McCoy
+# Institute for Nuclear Theory 
+#
+# SPDX-License-Identifier: MIT
+#
+# 3/29/22 (aem) : Created
+###############################################################################
+cmake_minimum_required(VERSION 3.20)
+
+include(FetchContent)
+
+###########################################################################
+# From where do you fetch
+###########################################################################
+FetchContent_Declare(
+  wigxjpf
+  GIT_REPOSITORY git@github.com:nd-nuclear-theory/wigxjpf.git
+  # Working commit as of (3/29/22) 15a6330dfe48c4fc85d6592fb892ef1468617fa5 
+  GIT_TAG        main 
+  GIT_SHALLOW    TRUE
+)
+
+FetchContent_Declare(
+  mpfun20
+  GIT_REPOSITORY git@github.com:nd-nuclear-theory/mpfun20-fort.git
+  GIT_TAG        master #0628af82d02c2e07e1e24f8b6005aef3c58b6cd1
+  GIT_SHALLOW    TRUE
+)
+
+###########################################################################
+# Retrieval function
+###########################################################################
+function(retrieve_package target package_name)
+  if(NOT TARGET ${target})
+    message(STATUS "Looking for package ${package_name}...")
+    find_package(${package_name})
+  endif()
+  ## if still not found, fetch
+  if(NOT TARGET ${target})
+    message(STATUS "Package ${package_name} not found. Fetching...")
+    
+    FetchContent_MakeAvailable(${package_name})
+  endif()
+endfunction()
+
+
