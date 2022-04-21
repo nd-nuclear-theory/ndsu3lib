10caa53841e5a58471b17dfa8b30641b8078fc7b
Mark A. Caprio <mcaprio-nd-edu>
2022-04-21 16:00:47 -0400

    Apply emacs F90 formatting to all fortran sources

--- ndsu3lib_example.f90
+++ ndsu3lib_example.f90
@@ -9,250 +9,250 @@
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 PROGRAM ndsu3lib_example
-!------------------------------------------------------------------------------------------------------------
-! Simple program calculating SU(3) reduced Wigner and recoupling coefficients and printing them on the screen
-!------------------------------------------------------------------------------------------------------------
-USE ndsu3lib_wigner_canonical
-USE ndsu3lib_wigner_su3so3
-USE ndsu3lib_recoupling
-IMPLICIT NONE
-TYPE(su3irrep) :: irrep1,irrep2,irrep3,irrep,irrep12,irrep23,irrep13,irrep4,irrep24,irrep34
-INTEGER :: epsilon3,Lambda3_twice,dimpq,dimw,rhomax,numb,i,p1,q1,p2,q2,epsilon1,Lambda1_twice,epsilon2,Lambda2_twice,&
-           rho,L1,L2,L3,kappa1max,kappa2max,kappa3max,kappa1,kappa2,kappa3,rhomaxa,rhomaxb,rhomaxc,rhomaxd,&
-           info,rhoa,rhob,rhoc,rhod,n,rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324,rho12,rho34,&
-           rho1234,rho13,rho24,rho1324
-INTEGER,ALLOCATABLE,DIMENSION(:) :: p1a,p2a,q2a
-REAL(KIND=8),ALLOCATABLE,DIMENSION(:) :: wigner_block
-REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:,:,:) :: wigner_phys
-REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:) :: rac,Zcoeff
-REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:,:,:,:,:) :: ninelm
+  !------------------------------------------------------------------------------------------------------------
+  ! Simple program calculating SU(3) reduced Wigner and recoupling coefficients and printing them on the screen
+  !------------------------------------------------------------------------------------------------------------
+  USE ndsu3lib_wigner_canonical
+  USE ndsu3lib_wigner_su3so3
+  USE ndsu3lib_recoupling
+  IMPLICIT NONE
+  TYPE(su3irrep) :: irrep1,irrep2,irrep3,irrep,irrep12,irrep23,irrep13,irrep4,irrep24,irrep34
+  INTEGER :: epsilon3,Lambda3_twice,dimpq,dimw,rhomax,numb,i,p1,q1,p2,q2,epsilon1,Lambda1_twice,epsilon2,Lambda2_twice,&
+       rho,L1,L2,L3,kappa1max,kappa2max,kappa3max,kappa1,kappa2,kappa3,rhomaxa,rhomaxb,rhomaxc,rhomaxd,&
+       info,rhoa,rhob,rhoc,rhod,n,rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324,rho12,rho34,&
+       rho1234,rho13,rho24,rho1324
+  INTEGER,ALLOCATABLE,DIMENSION(:) :: p1a,p2a,q2a
+  REAL(KIND=8),ALLOCATABLE,DIMENSION(:) :: wigner_block
+  REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:,:,:) :: wigner_phys
+  REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:) :: rac,Zcoeff
+  REAL(KIND=8),ALLOCATABLE,DIMENSION(:,:,:,:,:,:) :: ninelm
 
-CALL ndsu3lib_init(.TRUE.,400)
+  CALL ndsu3lib_init(.TRUE.,400)
 
-!************************************************************************************************************
-! SU(3)-SU(2)xU(1) reduced Wigner coefficients <(6,1)epsilon1,Lambda1;(2,1)epsilon2,Lambda2||(5,2)-9,5/2>_rho
-!************************************************************************************************************
+  !************************************************************************************************************
+  ! SU(3)-SU(2)xU(1) reduced Wigner coefficients <(6,1)epsilon1,Lambda1;(2,1)epsilon2,Lambda2||(5,2)-9,5/2>_rho
+  !************************************************************************************************************
 
-irrep1%lambda=6
-irrep1%mu=1
-irrep2%lambda=2
-irrep2%mu=1
-irrep3%lambda=5
-irrep3%mu=2
-epsilon3=-9
-Lambda3_twice=5 ! Lambda3_twice is 2*Lambda3
+  irrep1%lambda=6
+  irrep1%mu=1
+  irrep2%lambda=2
+  irrep2%mu=1
+  irrep3%lambda=5
+  irrep3%mu=2
+  epsilon3=-9
+  Lambda3_twice=5 ! Lambda3_twice is 2*Lambda3
 
-rhomax=outer_multiplicity(irrep1,irrep2,irrep3)
+  rhomax=outer_multiplicity(irrep1,irrep2,irrep3)
 
-dimpq=(MAX(irrep1%lambda,irrep1%mu)+1)*(irrep2%lambda+1)*(irrep2%mu+1)
-dimw=rhomax*dimpq
+  dimpq=(MAX(irrep1%lambda,irrep1%mu)+1)*(irrep2%lambda+1)*(irrep2%mu+1)
+  dimw=rhomax*dimpq
 
-ALLOCATE(wigner_block(dimw),p1a(dimpq),p2a(dimpq),q2a(dimpq))
+  ALLOCATE(wigner_block(dimw),p1a(dimpq),p2a(dimpq),q2a(dimpq))
 
-CALL calculate_wigner_canonical(irrep1,irrep2,irrep3,epsilon3,Lambda3_twice,dimpq,dimw,rhomax,numb,wigner_block,p1a,p2a,q2a)
+  CALL calculate_wigner_canonical(irrep1,irrep2,irrep3,epsilon3,Lambda3_twice,dimpq,dimw,rhomax,numb,wigner_block,p1a,p2a,q2a)
 
-WRITE(*,"(A)")"SU(3)-SU(2)xU(1) reduced Wigner coefficients <(6,1)epsilon1,Lambda1;(2,1)epsilon2,Lambda2||(5,2)-9,5/2>_rho:"
-WRITE(*,"(A,I1)")"epsilon1  2*Lambda1  epsilon2  2*Lambda2  coefficients for rho=1,...,rhomax=",rhomax
-DO i=1,numb
-  p1=p1a(i)
-  p2=p2a(i)
-  q2=q2a(i)
-  q1=(2*(irrep1%lambda+irrep2%lambda)+irrep1%mu+irrep2%mu-epsilon3)/3-p1-p2-q2
-  epsilon2=2*irrep2%lambda+irrep2%mu-3*(p2+q2)
-  epsilon1=epsilon3-epsilon2
-  Lambda1_twice=irrep1%mu+p1-q1 ! Lambda1_twice is 2*Lambda1
-  Lambda2_twice=irrep2%mu+p2-q2 ! Lambda2_twice is 2*Lambda2
-  WRITE(*,"(X,I4,7X,I3,7X,I4,7X,I3,3X,2D25.16)")epsilon1,Lambda1_twice,epsilon2,Lambda2_twice,&
-                                                (wigner_block(i+numb*(rho-1)),rho=1,rhomax)
-END DO
-
-DEALLOCATE(wigner_block,p1a,p2a,q2a)
-
-!*****************************************************************************************
-! SU(3)-SO(3) reduced Wigner coefficients <(6,1)kappa1,2;(2,1)kappa2,3||(5,2)kappa3,3>_rho
-!*****************************************************************************************
-
-L1=2
-L2=3
-L3=3
-kappa1max=inner_multiplicity(irrep1,L1)
-kappa2max=inner_multiplicity(irrep2,L2)
-kappa3max=inner_multiplicity(irrep3,L3)
-
-ALLOCATE(wigner_phys(kappa1max,kappa2max,kappa3max,rhomax))
-
-CALL calculate_wigner_su3so3(irrep1,L1,kappa1max,irrep2,L2,kappa2max,irrep3,L3,kappa3max,rhomax,wigner_phys)
-
-WRITE(*,*)
-WRITE(*,"(A)")"SU(3)-SO(3) reduced Wigner coefficients <(6,1)kappa1,2;(2,1)kappa2,3||(5,2)kappa3,3>_rho:"
-WRITE(*,"(A,I1)")"kappa1  kappa2  kappa3  coefficients for rho=1,...,rhomax=",rhomax
-DO kappa1=1,kappa1max
-  DO kappa2=1,kappa2max
-    DO kappa3=1,kappa3max
-      WRITE(*,"(2X,I2,6X,I2,6X,I2,X,2D25.16)")kappa1,kappa2,kappa3,(wigner_phys(kappa1,kappa2,kappa3,rho),rho=1,rhomax)
-    END DO
+  WRITE(*,"(A)")"SU(3)-SU(2)xU(1) reduced Wigner coefficients <(6,1)epsilon1,Lambda1;(2,1)epsilon2,Lambda2||(5,2)-9,5/2>_rho:"
+  WRITE(*,"(A,I1)")"epsilon1  2*Lambda1  epsilon2  2*Lambda2  coefficients for rho=1,...,rhomax=",rhomax
+  DO i=1,numb
+     p1=p1a(i)
+     p2=p2a(i)
+     q2=q2a(i)
+     q1=(2*(irrep1%lambda+irrep2%lambda)+irrep1%mu+irrep2%mu-epsilon3)/3-p1-p2-q2
+     epsilon2=2*irrep2%lambda+irrep2%mu-3*(p2+q2)
+     epsilon1=epsilon3-epsilon2
+     Lambda1_twice=irrep1%mu+p1-q1 ! Lambda1_twice is 2*Lambda1
+     Lambda2_twice=irrep2%mu+p2-q2 ! Lambda2_twice is 2*Lambda2
+     WRITE(*,"(X,I4,7X,I3,7X,I4,7X,I3,3X,2D25.16)")epsilon1,Lambda1_twice,epsilon2,Lambda2_twice,&
+          (wigner_block(i+numb*(rho-1)),rho=1,rhomax)
   END DO
-END DO
 
-DEALLOCATE(wigner_phys)
+  DEALLOCATE(wigner_block,p1a,p2a,q2a)
 
-!***********************************************************************************
-! SU(3) recoupling coefficients U[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]
-!***********************************************************************************
+  !*****************************************************************************************
+  ! SU(3)-SO(3) reduced Wigner coefficients <(6,1)kappa1,2;(2,1)kappa2,3||(5,2)kappa3,3>_rho
+  !*****************************************************************************************
 
-irrep1%lambda=9
-irrep1%mu=3
-irrep2%lambda=1
-irrep2%mu=1
-irrep%lambda=6
-irrep%mu=6
-irrep3%lambda=2
-irrep3%mu=2
-irrep12%lambda=9
-irrep12%mu=3
-irrep23%lambda=3
-irrep23%mu=3
+  L1=2
+  L2=3
+  L3=3
+  kappa1max=inner_multiplicity(irrep1,L1)
+  kappa2max=inner_multiplicity(irrep2,L2)
+  kappa3max=inner_multiplicity(irrep3,L3)
 
-rhomaxa=outer_multiplicity(irrep1,irrep2,irrep12)
-rhomaxb=outer_multiplicity(irrep12,irrep3,irrep)
-rhomaxc=outer_multiplicity(irrep2,irrep3,irrep23)
-rhomaxd=outer_multiplicity(irrep1,irrep23,irrep)
+  ALLOCATE(wigner_phys(kappa1max,kappa2max,kappa3max,rhomax))
 
-ALLOCATE(rac(rhomaxd,rhomaxa*rhomaxb*rhomaxc))
+  CALL calculate_wigner_su3so3(irrep1,L1,kappa1max,irrep2,L2,kappa2max,irrep3,L3,kappa3max,rhomax,wigner_phys)
 
-CALL calculate_u_coeff(irrep1,irrep2,irrep,irrep3,irrep12,irrep23,rhomaxa,rhomaxb,rhomaxc,rhomaxd,rac,rhomaxd,info)
-
-WRITE(*,*)
-IF(info==0)THEN
-  WRITE(*,"(A)")"SU(3) recoupling coefficients U[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]:"
-  WRITE(*,"(A)")"rhoa  rhob  rhoc  rhod  coefficient"
-  DO rhoa=1,rhomaxa
-    DO rhob=1,rhomaxb
-      DO rhoc=1,rhomaxc
-        DO rhod=1,rhomaxd
-          n=rhoa+rhomaxa*(rhob-1)+rhomaxa*rhomaxb*(rhoc-1)
-          WRITE(*,"(X,I2,4X,I2,4X,I2,4X,I2,D25.16)")rhoa,rhob,rhoc,rhod,rac(rhod,n)
+  WRITE(*,*)
+  WRITE(*,"(A)")"SU(3)-SO(3) reduced Wigner coefficients <(6,1)kappa1,2;(2,1)kappa2,3||(5,2)kappa3,3>_rho:"
+  WRITE(*,"(A,I1)")"kappa1  kappa2  kappa3  coefficients for rho=1,...,rhomax=",rhomax
+  DO kappa1=1,kappa1max
+     DO kappa2=1,kappa2max
+        DO kappa3=1,kappa3max
+           WRITE(*,"(2X,I2,6X,I2,6X,I2,X,2D25.16)")kappa1,kappa2,kappa3,(wigner_phys(kappa1,kappa2,kappa3,rho),rho=1,rhomax)
         END DO
-      END DO
-    END DO
+     END DO
   END DO
-ELSE
-  WRITE(*,"(A,I2)")"calculate_u_coeff: MKL subroutine dgesv ran with error: info=",info
-END IF
 
-DEALLOCATE(rac)
+  DEALLOCATE(wigner_phys)
 
-!***********************************************************************************
-! SU(3) recoupling coefficients Z[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]
-!***********************************************************************************
+  !***********************************************************************************
+  ! SU(3) recoupling coefficients U[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]
+  !***********************************************************************************
 
-irrep2%lambda=9
-irrep2%mu=3
-irrep1%lambda=1
-irrep1%mu=1
-irrep%lambda=6
-irrep%mu=6
-irrep3%lambda=2
-irrep3%mu=2
-irrep12%lambda=9
-irrep12%mu=3
-irrep13%lambda=3
-irrep13%mu=3
+  irrep1%lambda=9
+  irrep1%mu=3
+  irrep2%lambda=1
+  irrep2%mu=1
+  irrep%lambda=6
+  irrep%mu=6
+  irrep3%lambda=2
+  irrep3%mu=2
+  irrep12%lambda=9
+  irrep12%mu=3
+  irrep23%lambda=3
+  irrep23%mu=3
 
-rhomaxa=outer_multiplicity(irrep1,irrep2,irrep12)
-rhomaxb=outer_multiplicity(irrep12,irrep3,irrep)
-rhomaxc=outer_multiplicity(irrep1,irrep3,irrep13)
-rhomaxd=outer_multiplicity(irrep13,irrep2,irrep)
+  rhomaxa=outer_multiplicity(irrep1,irrep2,irrep12)
+  rhomaxb=outer_multiplicity(irrep12,irrep3,irrep)
+  rhomaxc=outer_multiplicity(irrep2,irrep3,irrep23)
+  rhomaxd=outer_multiplicity(irrep1,irrep23,irrep)
 
-ALLOCATE(Zcoeff(rhomaxd,rhomaxa*rhomaxb*rhomaxc))
+  ALLOCATE(rac(rhomaxd,rhomaxa*rhomaxb*rhomaxc))
 
-CALL calculate_z_coeff(irrep2,irrep1,irrep,irrep3,irrep12,irrep13,rhomaxa,rhomaxb,rhomaxc,rhomaxd,Zcoeff,rhomaxd,info)
+  CALL calculate_u_coeff(irrep1,irrep2,irrep,irrep3,irrep12,irrep23,rhomaxa,rhomaxb,rhomaxc,rhomaxd,rac,rhomaxd,info)
 
-WRITE(*,*)
-IF(info==0)THEN
-  WRITE(*,"(A)")"SU(3) recoupling coefficients Z[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]:"
-  WRITE(*,"(A)")"rhoa  rhob  rhoc  rhod  coefficient"
-  DO rhoa=1,rhomaxa
-    DO rhob=1,rhomaxb
-      DO rhoc=1,rhomaxc
-        DO rhod=1,rhomaxd
-          n=rhoa+rhomaxa*(rhob-1)+rhomaxa*rhomaxb*(rhoc-1)
-          WRITE(*,"(X,I2,4X,I2,4X,I2,4X,I2,D25.16)")rhoa,rhob,rhoc,rhod,Zcoeff(rhod,n)
+  WRITE(*,*)
+  IF(info==0)THEN
+     WRITE(*,"(A)")"SU(3) recoupling coefficients U[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]:"
+     WRITE(*,"(A)")"rhoa  rhob  rhoc  rhod  coefficient"
+     DO rhoa=1,rhomaxa
+        DO rhob=1,rhomaxb
+           DO rhoc=1,rhomaxc
+              DO rhod=1,rhomaxd
+                 n=rhoa+rhomaxa*(rhob-1)+rhomaxa*rhomaxb*(rhoc-1)
+                 WRITE(*,"(X,I2,4X,I2,4X,I2,4X,I2,D25.16)")rhoa,rhob,rhoc,rhod,rac(rhod,n)
+              END DO
+           END DO
         END DO
-      END DO
-    END DO
-  END DO
-ELSE
-  WRITE(*,"(A,I2)")"calculate_z_coeff: MKL subroutine dgesv ran with error: info=",info
-END IF
+     END DO
+  ELSE
+     WRITE(*,"(A,I2)")"calculate_u_coeff: MKL subroutine dgesv ran with error: info=",info
+  END IF
 
-DEALLOCATE(Zcoeff)
+  DEALLOCATE(rac)
 
-!********************************************************
-!                            |(1,1) (0,0)  (1,1)  rho12 |
-! 9-(lambda,mu) coefficients |(0,4) (4,2)  (2,4)  rho34 |
-!                            |(2,3) (4,2)  (2,4) rho1324|
-!                            |rho13 rho24 rho1234       |
-!********************************************************
+  !***********************************************************************************
+  ! SU(3) recoupling coefficients Z[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]
+  !***********************************************************************************
 
-irrep1%lambda=1
-irrep1%mu=1
-irrep2%lambda=0
-irrep2%mu=0
-irrep12%lambda=1
-irrep12%mu=1
-irrep3%lambda=0
-irrep3%mu=4
-irrep4%lambda=4
-irrep4%mu=2
-irrep34%lambda=2
-irrep34%mu=4
-irrep13%lambda=2
-irrep13%mu=3
-irrep24%lambda=4
-irrep24%mu=2
-irrep%lambda=2
-irrep%mu=4
+  irrep2%lambda=9
+  irrep2%mu=3
+  irrep1%lambda=1
+  irrep1%mu=1
+  irrep%lambda=6
+  irrep%mu=6
+  irrep3%lambda=2
+  irrep3%mu=2
+  irrep12%lambda=9
+  irrep12%mu=3
+  irrep13%lambda=3
+  irrep13%mu=3
 
-rhomax12=outer_multiplicity(irrep1,irrep2,irrep12)
-rhomax34=outer_multiplicity(irrep3,irrep4,irrep34)
-rhomax1234=outer_multiplicity(irrep12,irrep34,irrep)
-rhomax13=outer_multiplicity(irrep1,irrep3,irrep13)
-rhomax24=outer_multiplicity(irrep2,irrep4,irrep24)
-rhomax1324=outer_multiplicity(irrep13,irrep24,irrep)
+  rhomaxa=outer_multiplicity(irrep1,irrep2,irrep12)
+  rhomaxb=outer_multiplicity(irrep12,irrep3,irrep)
+  rhomaxc=outer_multiplicity(irrep1,irrep3,irrep13)
+  rhomaxd=outer_multiplicity(irrep13,irrep2,irrep)
 
-ALLOCATE(ninelm(rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324))
+  ALLOCATE(Zcoeff(rhomaxd,rhomaxa*rhomaxb*rhomaxc))
 
-CALL calculate_9_lambda_mu(irrep1,irrep2,irrep12,irrep3,irrep4,irrep34,irrep13,irrep24,irrep,&
-                           rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324,ninelm,info)
+  CALL calculate_z_coeff(irrep2,irrep1,irrep,irrep3,irrep12,irrep13,rhomaxa,rhomaxb,rhomaxc,rhomaxd,Zcoeff,rhomaxd,info)
 
-WRITE(*,*)
-IF(info==0)THEN
-  WRITE(*,"(27X,A)")"|(1,1) (0,0)  (1,1)  rho12 |"
-  WRITE(*,"(A)")"9-(lambda,mu) coefficients |(0,4) (4,2)  (2,4)  rho34 |:"
-  WRITE(*,"(27X,A)")"|(2,3) (4,2)  (2,4) rho1324|"
-  WRITE(*,"(27X,A)")"|rho13 rho24 rho1234       |"
-  WRITE(*,"(A)")"rho12  rho34  rho1234  rho13  rho24  rho1324  coefficient"
-  DO rho12=1,rhomax12
-    DO rho34=1,rhomax34
-      DO rho1234=1,rhomax1234
-        DO rho13=1,rhomax13
-          DO rho24=1,rhomax24
-            DO rho1324=1,rhomax1324
-              WRITE(*,"(X,I2,5X,I2,6X,I2,6X,I2,5X,I2,6X,I2,2X,D25.16)")rho12,rho34,rho1234,rho13,rho24,rho1324,&
-                                                                       ninelm(rho12,rho34,rho1234,rho13,rho24,rho1324)
-            END DO
-          END DO
+  WRITE(*,*)
+  IF(info==0)THEN
+     WRITE(*,"(A)")"SU(3) recoupling coefficients Z[(9,3)(1,1)(6,6)(2,2);(9,3)rhoa,rhob(3,3)rhoc,rhod]:"
+     WRITE(*,"(A)")"rhoa  rhob  rhoc  rhod  coefficient"
+     DO rhoa=1,rhomaxa
+        DO rhob=1,rhomaxb
+           DO rhoc=1,rhomaxc
+              DO rhod=1,rhomaxd
+                 n=rhoa+rhomaxa*(rhob-1)+rhomaxa*rhomaxb*(rhoc-1)
+                 WRITE(*,"(X,I2,4X,I2,4X,I2,4X,I2,D25.16)")rhoa,rhob,rhoc,rhod,Zcoeff(rhod,n)
+              END DO
+           END DO
         END DO
-      END DO
-    END DO
-  END DO
-ELSE
-  WRITE(*,"(A,I2)")"calculate_9_lambda_mu: MKL subroutine dgesv ran with error: info=",info
-END IF
+     END DO
+  ELSE
+     WRITE(*,"(A,I2)")"calculate_z_coeff: MKL subroutine dgesv ran with error: info=",info
+  END IF
 
-DEALLOCATE(ninelm)
+  DEALLOCATE(Zcoeff)
 
-CALL ndsu3lib_free(.TRUE.)
+  !********************************************************
+  !                            |(1,1) (0,0)  (1,1)  rho12 |
+  ! 9-(lambda,mu) coefficients |(0,4) (4,2)  (2,4)  rho34 |
+  !                            |(2,3) (4,2)  (2,4) rho1324|
+  !                            |rho13 rho24 rho1234       |
+  !********************************************************
+
+  irrep1%lambda=1
+  irrep1%mu=1
+  irrep2%lambda=0
+  irrep2%mu=0
+  irrep12%lambda=1
+  irrep12%mu=1
+  irrep3%lambda=0
+  irrep3%mu=4
+  irrep4%lambda=4
+  irrep4%mu=2
+  irrep34%lambda=2
+  irrep34%mu=4
+  irrep13%lambda=2
+  irrep13%mu=3
+  irrep24%lambda=4
+  irrep24%mu=2
+  irrep%lambda=2
+  irrep%mu=4
+
+  rhomax12=outer_multiplicity(irrep1,irrep2,irrep12)
+  rhomax34=outer_multiplicity(irrep3,irrep4,irrep34)
+  rhomax1234=outer_multiplicity(irrep12,irrep34,irrep)
+  rhomax13=outer_multiplicity(irrep1,irrep3,irrep13)
+  rhomax24=outer_multiplicity(irrep2,irrep4,irrep24)
+  rhomax1324=outer_multiplicity(irrep13,irrep24,irrep)
+
+  ALLOCATE(ninelm(rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324))
+
+  CALL calculate_9_lambda_mu(irrep1,irrep2,irrep12,irrep3,irrep4,irrep34,irrep13,irrep24,irrep,&
+       rhomax12,rhomax34,rhomax1234,rhomax13,rhomax24,rhomax1324,ninelm,info)
+
+  WRITE(*,*)
+  IF(info==0)THEN
+     WRITE(*,"(27X,A)")"|(1,1) (0,0)  (1,1)  rho12 |"
+     WRITE(*,"(A)")"9-(lambda,mu) coefficients |(0,4) (4,2)  (2,4)  rho34 |:"
+     WRITE(*,"(27X,A)")"|(2,3) (4,2)  (2,4) rho1324|"
+     WRITE(*,"(27X,A)")"|rho13 rho24 rho1234       |"
+     WRITE(*,"(A)")"rho12  rho34  rho1234  rho13  rho24  rho1324  coefficient"
+     DO rho12=1,rhomax12
+        DO rho34=1,rhomax34
+           DO rho1234=1,rhomax1234
+              DO rho13=1,rhomax13
+                 DO rho24=1,rhomax24
+                    DO rho1324=1,rhomax1324
+                       WRITE(*,"(X,I2,5X,I2,6X,I2,6X,I2,5X,I2,6X,I2,2X,D25.16)")rho12,rho34,rho1234,rho13,rho24,rho1324,&
+                            ninelm(rho12,rho34,rho1234,rho13,rho24,rho1324)
+                    END DO
+                 END DO
+              END DO
+           END DO
+        END DO
+     END DO
+  ELSE
+     WRITE(*,"(A,I2)")"calculate_9_lambda_mu: MKL subroutine dgesv ran with error: info=",info
+  END IF
+
+  DEALLOCATE(ninelm)
+
+  CALL ndsu3lib_free(.TRUE.)
 
 END PROGRAM ndsu3lib_example
--- rwlock_example.f90
+++ rwlock_example.f90
@@ -6,94 +6,94 @@
 ! array is sufficient and resizes the array if needed. It uses the rwlock.
 
 MODULE modul
-! This module represents ndsu3lib. It contains an array, a subroutine allocating
-! the array and filling it with non-negative integers, and a subroutine using the array.
-USE rwlock
-IMPLICIT NONE
-INTEGER,ALLOCATABLE,DIMENSION(:) :: array
-INTEGER :: upbound_array ! the size of `array`
-TYPE(ReadWriteLock_t) :: lock
+  ! This module represents ndsu3lib. It contains an array, a subroutine allocating
+  ! the array and filling it with non-negative integers, and a subroutine using the array.
+  USE rwlock
+  IMPLICIT NONE
+  INTEGER,ALLOCATABLE,DIMENSION(:) :: array
+  INTEGER :: upbound_array ! the size of `array`
+  TYPE(ReadWriteLock_t) :: lock
 CONTAINS
 
-SUBROUTINE allocate_array(upbound)
-! allocates `array` and fills it with non-negative integers
-IMPLICIT NONE
-INTEGER,INTENT(IN) :: upbound
-INTEGER :: i
-upbound_array=upbound
-ALLOCATE(array(upbound_array))
-DO i=1,upbound_array
-  array(i)=i
-END DO
-END SUBROUTINE allocate_array
+  SUBROUTINE allocate_array(upbound)
+    ! allocates `array` and fills it with non-negative integers
+    IMPLICIT NONE
+    INTEGER,INTENT(IN) :: upbound
+    INTEGER :: i
+    upbound_array=upbound
+    ALLOCATE(array(upbound_array))
+    DO i=1,upbound_array
+       array(i)=i
+    END DO
+  END SUBROUTINE allocate_array
 
-SUBROUTINE routine(numb,suma)
-! calculates the sum of the first `numb` elements of `array`
-IMPLICIT NONE
-INTEGER,INTENT(IN) :: numb
-INTEGER,INTENT(OUT) :: suma
-INTEGER :: i
+  SUBROUTINE routine(numb,suma)
+    ! calculates the sum of the first `numb` elements of `array`
+    IMPLICIT NONE
+    INTEGER,INTENT(IN) :: numb
+    INTEGER,INTENT(OUT) :: suma
+    INTEGER :: i
 
-! acquire a reader lock
-CALL lock%reader_lock()
-
-! if we need to resize `array`, try to get a writer lock
-DO WHILE(upbound_array<numb)
-  IF(.NOT.lock%writer_lock(.TRUE.))THEN
-    ! if we didn't get the writer lock, some other thread is resizing
-    ! the array -- wait for it to finish, and re-check if resizing is
-    ! still required
-    CALL lock%reader_unlock()
+    ! acquire a reader lock
     CALL lock%reader_lock()
-    ! flush, to get update from other thread which did update
-    !$omp flush acquire
-    CYCLE
-  END IF
-  ! flush, to get any updates from other writers
-  !$omp flush acquire
 
-  ! resize the array
-  DEALLOCATE(array)
-  CALL allocate_array(upbound_array+10)
-  ! flush, to publish update to other threads
-  !$omp flush release
+    ! if we need to resize `array`, try to get a writer lock
+    DO WHILE(upbound_array<numb)
+       IF(.NOT.lock%writer_lock(.TRUE.))THEN
+          ! if we didn't get the writer lock, some other thread is resizing
+          ! the array -- wait for it to finish, and re-check if resizing is
+          ! still required
+          CALL lock%reader_unlock()
+          CALL lock%reader_lock()
+          ! flush, to get update from other thread which did update
+          !$omp flush acquire
+          CYCLE
+       END IF
+       ! flush, to get any updates from other writers
+       !$omp flush acquire
 
-  ! release the writer lock
-  CALL lock%writer_unlock(.TRUE.)
-END DO
+       ! resize the array
+       DEALLOCATE(array)
+       CALL allocate_array(upbound_array+10)
+       ! flush, to publish update to other threads
+       !$omp flush release
 
-suma=0
-DO i=1,numb
-  suma=suma+array(i)
-END DO
+       ! release the writer lock
+       CALL lock%writer_unlock(.TRUE.)
+    END DO
 
-! release the reader lock
-CALL lock%reader_unlock()
+    suma=0
+    DO i=1,numb
+       suma=suma+array(i)
+    END DO
 
-END SUBROUTINE routine
+    ! release the reader lock
+    CALL lock%reader_unlock()
+
+  END SUBROUTINE routine
 
 END MODULE modul
 
 PROGRAM rwlock_example
-! This program represents a program that calls ndsu3lib.
-USE modul
-USE omp_lib
-IMPLICIT NONE
-INTEGER :: numb,suma
+  ! This program represents a program that calls ndsu3lib.
+  USE modul
+  USE omp_lib
+  IMPLICIT NONE
+  INTEGER :: numb,suma
 
-CALL allocate_array(10)
+  CALL allocate_array(10)
 
-! initialize the lock
-CALL lock%init()
+  ! initialize the lock
+  CALL lock%init()
 
-!$omp parallel do schedule(static) default(none) &
-!$omp     private(numb, suma) &
-!$omp     shared(lock, upbound_array, array)
-DO numb=1,50
-  CALL routine(numb,suma)
-END DO
-!$omp end parallel do
+  !$omp parallel do schedule(static) default(none) &
+  !$omp     private(numb, suma) &
+  !$omp     shared(lock, upbound_array, array)
+  DO numb=1,50
+     CALL routine(numb,suma)
+  END DO
+  !$omp end parallel do
 
-DEALLOCATE(array)
+  DEALLOCATE(array)
 
 END PROGRAM rwlock_example
