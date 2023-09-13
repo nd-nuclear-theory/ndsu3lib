2e3eb316e21b056f824dacbf052e605e23fcc74d
Mark A. Caprio <mcaprio-nd-edu>
2023-09-13 17:48:39 -0500

    Rename ndsu3lib_example.cpp to ndsu3lib_example_cpp.cpp

--- module.mk
+++ module.mk
@@ -7,7 +7,7 @@ $(eval $(begin-module))
 # module_units_h := 
 module_units_cpp-h := c++wrappers
 module_units_f := ndsu3lib_recoupling ndsu3lib_wigner_canonical ndsu3lib_wigner_su3so3
-module_programs_cpp := ndsu3lib_example
+module_programs_cpp := ndsu3lib_example_cpp
 module_programs_f := ndsu3lib_example 
 # module_programs_f_test := # DOES NOT EXIST YET
 # module_generated :=
